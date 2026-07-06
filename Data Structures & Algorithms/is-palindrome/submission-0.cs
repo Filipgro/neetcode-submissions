28ae13e52d2e87e84b670d7da95ac37530722fc2
Filipgro <153622966+Filipgro@users.noreply.github.com>
2026-07-06 14:49:57 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        s= s.ToLower();
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (!char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+            else if (!char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+            else if (s[left] != s[right])
+            {
+                return false;
+            }
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+        return true;    
+    }
+}
