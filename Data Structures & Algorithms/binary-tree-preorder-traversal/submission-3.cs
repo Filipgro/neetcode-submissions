3eb3ae36b28582b524d05b1fad74c8535cd2340c
Filipgro <153622966+Filipgro@users.noreply.github.com>
2026-09-01 20:18:24 +0300

    Add: binary-tree-preorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-3.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<int> PreorderTraversal(TreeNode root) {
+        List<int> res=new List<int>();
+        dfs(root,res);
+        return res;
+    }
+    private void dfs(TreeNode node,List<int> res){
+        if(node==null){
+            return;
+        }
+        res.Add(node.val);
+        dfs(node.left,res);
+        dfs(node.right,res);
+    }
+}
